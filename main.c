f2e0155ead23dc9d5d3eeb6a061976502662d293
tim108108 <tim108108@gmail.com>
2024-01-09 22:24:47 +0800

    add top exit app

--- main.c
+++ main.c
@@ -15,8 +15,8 @@ typedef struct point_console
     char count;
     char argc;
     char *argv[CON_BUF_MAX_SIZE];
+    char flag;
 }pt_console;
-
 typedef struct console_command_st
 {
     char cmd[CON_BUF_MAX_SIZE];
@@ -28,21 +28,53 @@ void console_getchar(pt_console *console);
 void console_tabcomplete(pt_console *console);
 void console_getopt(pt_console *console);
 void console_shell(pt_console *console);
-
 /* ======== app ======== */
-void ls_app(char argc, char **argv)
+pt_console gconsole;
+void app_ls(char argc, char **argv)
 {
     printf("\napp:ls");
+    return;
 }
-void cd_app(char argc, char **argv)
+void app_cd(char argc, char **argv)
 {
     printf("\napp:cd");
+    return;
+}
+void app_prt(char argc, char **argv)
+{
+    int i = 0;
+    printf("\n");
+    for (i = 0; i < strlen(argv[1]) + 4; i++){
+        printf("=");
+    }
+    printf("\n= %s =\n",argv[1]);
+    for (i = 0; i < strlen(argv[1]) + 4; i++){
+        printf("=");
+    }
+    return;
+}
+void app_top(char argc, char **argv)
+{
+    printf("\ncount:%d", gconsole.count);
+    printf("\nargc:%d", gconsole.argc);
+    printf("\nflag:%x", gconsole.flag);
+    return;
+}
+void app_exit(char argc, char **argv)
+{
+    gconsole.flag = 0;
+    printf("\n");
+    printf("Function End~");
+    return;
 }
 /* ======== cmd ======== */
 console_cmd_arr console_cmd[] = 
 {
-    {"ls", ls_app},
-    {"cd", cd_app},
+    {"ls", app_ls},
+    {"cd", app_cd},
+    {"print", app_prt},
+    {"top", app_top},
+    {"exit", app_exit},
     {"\0", NULL}
 };
 void console_reset(pt_console *console)
@@ -51,6 +83,7 @@ void console_reset(pt_console *console)
     console->count = 0;
     console->argc = 0;
     console->argv[0] = 0;
+    console->flag = 0xff;
     printf("\n$~");
 }
 void console_getchar(pt_console *console)
@@ -77,11 +110,9 @@ void console_getchar(pt_console *console)
     }
 }
 void console_tabcomplete(pt_console *console) 
-{
-    // 根據console->buffer去尋找console_cmd[]是否有匹配，並將對應的cmd寫入console->buffer並printf()
+{   // 根據console->buffer去尋找console_cmd[]是否有匹配，並將對應的cmd寫入console->buffer並printf()
     int index = 0, i;
     char partial_cmd[CON_BUF_MAX_SIZE];
-
     if(console->count < 1){
         printf("\nCommand List:");
         while (console_cmd[index].fp != NULL){
@@ -124,8 +155,13 @@ void console_getopt(pt_console *console)
 void console_shell(pt_console *console)
 {
     int i = 0;
+    // if (!strcmp(console->buffer, "exit")){
+    //     console->flag = 0;
+    //     printf("\n");
+    //     printf("Function End~");
+    //     return;
+    // }
     while (console_cmd[i].fp != NULL){
-        // printf("\n%s",console_cmd[i].cmd);
         if (!strcmp(console->argv[0], console_cmd[i].cmd)){
             console_cmd[i].fp(console->argc, console->argv);
             return;
@@ -133,27 +169,20 @@ void console_shell(pt_console *console)
         i++;
     }
 }
-
+/* ======== main function ======== */
 int main()
 {
     stdio_init_all();
 
-    pt_console *pconsole = malloc(sizeof(pt_console));
-    int i;
-
+    // pt_console *pconsole = malloc(sizeof(pt_console));
+    pt_console *pconsole = &gconsole;
     // sleep_ms(5000);
-    
-    while(1){
-
+    pconsole->flag = 1; //set flag before loop
+    while(pconsole->flag){
         console_reset(pconsole);        
         console_getchar(pconsole);
         console_getopt(pconsole);
         console_shell(pconsole);
-        if (!strcmp(pconsole->buffer, "exit")){
-            break;
-        }
-        // printf("\nrepeat:%s",pconsole->buffer);
     }
-    printf("\nfunction_end");
     return 0;
 }
